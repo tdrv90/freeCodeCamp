9805a27a4624578a674c01b49c3ab0916fe7cfe8
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-01-23 20:57:06 +0200

    Basic JavaScript.

---
+++ JavaScript Algorithms and Data Structures Certification (300 hours)/Basic JavaScript/77. Returning Boolean Values from FunctionsPassed.js
@@ -0,0 +1,15 @@
+// Fix the function isLess to remove the if/else statements.
+
+function isLess(a, b) {
+    // Fix this code
+    // if (a < b) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
