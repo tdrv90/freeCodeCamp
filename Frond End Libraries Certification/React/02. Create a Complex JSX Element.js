b7530be088d3c88b19b4a9476ad64c197fd0a2a4
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-07-19 14:45:49 +0300

    add React exercises 1-18

---
+++ Frond End Libraries Certification/React/02. Create a Complex JSX Element.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+    <h1>Some H1</h1>
+    <p>Some P</p>
+    <ul>
+        <li>Some LI1</li>
+        <li>Some LI2</li>
+        <li>Some LI3</li>
+    </ul>
+</div>
---
+++ Frond End Libraries Certification/React/07. Create a Stateless Functional Component.js
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+    // change code below this line
+    return (
+        <div>Some text...</div>
+    )
+    // change code above this line
+}
